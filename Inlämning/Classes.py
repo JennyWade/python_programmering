cc2320bd7a5925eb2be93b2c548d3b50f384b170
JennyWade <97337508+JennyWade@users.noreply.github.com>
2022-02-01 15:06:27 +0100

    New

--- Inlämning/Classes.py
+++ Inlämning/Classes.py
@@ -1,12 +1,21 @@
 from importlib.resources import path
+from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import sqlite3
 
 class clean_csv:
-    def import_csv(self, db):
-        self.db = pd.read_csv(path)
+    def __init__(self):
+        path = "/Users/jenny.wade@schibsted.com/Documents/GitHub/python_programmering/Assignment/vaccin_covid.csv"
+        self.df = pd.read_csv(path)
 
-    import_csv("C:/Users/jenny/Documents/GitHub/python_programmering/Assignment/vaccin_covid.csv")
-    
+    def _split(self):
+        #split vaccine column and get a list of unique values
+
+    def _remove(self):
+        #Remove columns
+        self.df = self.df.drop(["vaccines", "daily_vaccinations_raw"], axis=1, inplace=True)
+
+
+if __name__ == "__main__":
